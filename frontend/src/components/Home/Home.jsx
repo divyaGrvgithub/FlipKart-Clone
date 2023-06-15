ee967779e951ab0ae602540b41c365550c18dd00
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:03:04 +0530

    Update Home.jsx

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import Categories from '../Layouts/Categories';
 import Banner from './Banner/Banner';
-import DealSlider from './DealSlider/DealSlider';
+import DealSlider from './DealSlider/DealSlider'; 
 import ProductSlider from './ProductSlider/ProductSlider';
 import { useDispatch, useSelector } from 'react-redux'; 
 import { clearErrors, getSliderProducts } from '../../actions/productAction';
