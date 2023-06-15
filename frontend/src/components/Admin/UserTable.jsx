c451e08ed5693642560d3d4476d4b097c5a9baef
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:59:21 +0530

    Update UserTable.jsx

--- frontend/src/components/Admin/UserTable.jsx
+++ frontend/src/components/Admin/UserTable.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSnackbar } from 'notistack';
-import { clearErrors, deleteUser, getAllUsers } from '../../actions/userAction';
+import { clearErrors, deleteUser, getAllUsers } from '../../actions/userAction'; 
 import { DELETE_USER_RESET } from '../../constants/userConstants';
 import Actions from './Actions';
 import MetaData from '../Layouts/MetaData';
