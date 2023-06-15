49e2688f0c6196746c0c3ad3089da7f0da1cd747
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:09:11 +0530

    Update NotFound.jsx

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 import notFound from '../assets/images/404-not-found.svg';
 import { Link } from 'react-router-dom';
-
+ 
 const NotFound = () => {
     return ( 
         <div className="mt-16 flex flex-col gap-4 items-center justify-center">
