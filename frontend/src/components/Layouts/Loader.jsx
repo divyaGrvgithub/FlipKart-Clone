509fa27d8f43f91bc552c659e583f10971ea62ba
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:04:38 +0530

    Update Loader.jsx

--- frontend/src/components/Layouts/Loader.jsx
+++ frontend/src/components/Layouts/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
       <div className="min-h-screen min-w-full flex items-center justify-center">
           <CircularProgress />
       </div>
-  ); 
+  );  
 };
  
 export default Loader;
