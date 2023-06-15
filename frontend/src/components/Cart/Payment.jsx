c6521da4f87adf207596cc52f6b968a072b68988
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:00:49 +0530

    Update Payment.jsx

--- frontend/src/components/Cart/Payment.jsx
+++ frontend/src/components/Cart/Payment.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; 
 import PriceSidebar from './PriceSidebar';
 import Stepper from './Stepper';
 // import {
