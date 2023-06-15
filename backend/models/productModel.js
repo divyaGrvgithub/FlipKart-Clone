7b34c7ca16149e07f69acb4db921503d5a66443e
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:18:50 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: {
-        type: String, 
+        type: String,  
         required: [true, "Please enter product name"],
         trim: true
     }, 
