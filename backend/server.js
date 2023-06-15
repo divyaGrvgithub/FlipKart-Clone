8a32294c92282af1efc6f28a344dcd42fb42316d
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:53:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary');
 const PORT = process.env.PORT || 4000;  
   
 // UncaughtException Error  
-process.on('uncaughtException', (err) => {   
+process.on('uncaughtException', (err) => {    
     console.log(`Error: ${err.message}`);  
     process.exit(1);  
 });  
