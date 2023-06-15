158ec4a9c5a100ad115686c4fe2e268a891f5157
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:53:38 +0530

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,6 +1,6 @@
 const sendToken = (user, statusCode, res) => {  
     const token = user.getJWTToken();  
-  
+   
     const options = {  
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
