9014f68ce501d0a4c94fc6b21b55b3c4c5abf86f
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:08:14 +0530

    Update Sidebar.jsx

--- frontend/src/components/User/Sidebar.jsx
+++ frontend/src/components/User/Sidebar.jsx
@@ -5,7 +5,7 @@ import PersonIcon from '@mui/icons-material/Person';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet'; 
 import ChatIcon from '@mui/icons-material/Chat';
 import FolderSharedIcon from '@mui/icons-material/FolderShared';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew'; 
 import { Link, useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 import { logoutUser } from '../../actions/userAction';
