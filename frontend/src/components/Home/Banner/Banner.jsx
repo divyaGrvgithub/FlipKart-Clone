925f6288efa2d4710372bb342f835e5431a87c07
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:02:03 +0530

    Update Banner.jsx

--- frontend/src/components/Home/Banner/Banner.jsx
+++ frontend/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"; 
+import "slick-carousel/slick/slick-theme.css";  
 import './Banner.css';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
