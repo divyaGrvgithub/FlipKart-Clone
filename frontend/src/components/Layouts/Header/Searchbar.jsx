1f9f4c4d6b9dafe59db106a1126faf9c49d49174
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 18:03:51 +0530

    Update Searchbar.jsx

--- frontend/src/components/Layouts/Header/Searchbar.jsx
+++ frontend/src/components/Layouts/Header/Searchbar.jsx
@@ -1,6 +1,6 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 
 const Searchbar = () => {
  
