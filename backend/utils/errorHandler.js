2c6641c835341011f502445bf2e3e4c11d888ced
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:53:08 +0530

    Update errorHandler.js

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,7 +1,7 @@
 class ErrorHandler extends Error {  
     constructor(message, statusCode) {  
         super(message);   
-        this.statusCode = statusCode  
+        this.statusCode = statusCode   
    
         Error.captureStackTrace(this, this.constructor);   
     }  
