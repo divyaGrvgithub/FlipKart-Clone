b9f61f5fb3b2694ee31c8844e6eea040f83fbd89
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:52:56 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,7 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.route('/register').post(registerUser);
+router.route('/register').post(registerUser); 
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
  
