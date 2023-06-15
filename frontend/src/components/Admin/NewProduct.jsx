609fe932f079ff6f9a8c4de2233cc9faf5748ffa
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:57:58 +0530

    Update NewProduct.jsx

--- frontend/src/components/Admin/NewProduct.jsx
+++ frontend/src/components/Admin/NewProduct.jsx
@@ -2,7 +2,7 @@ import TextField from '@mui/material/TextField';
 import { useState, useEffect } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MenuItem from '@mui/material/MenuItem';
-import { useDispatch, useSelector } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux';  
 import { useSnackbar } from 'notistack';
 import { useNavigate } from 'react-router-dom';
 import { NEW_PRODUCT_RESET } from '../../constants/productConstants';
