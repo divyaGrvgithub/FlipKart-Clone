0276d1968659fa6b484034840cead0c9b8861f7d
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:54:58 +0530

    Update ProtectedRoute.js

--- frontend/src/Routes/ProtectedRoute.js
+++ frontend/src/Routes/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';     
 
-const ProtectedRoute = ({ children, isAdmin }) => {
+const ProtectedRoute = ({ children, isAdmin }) => { 
 
     const { loading, isAuthenticated, user } = useSelector(state => state.user);
 
