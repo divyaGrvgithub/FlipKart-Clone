5d39f6cc93ea0852f7c86bb9df7e6e3c96e10bf3
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-15 17:18:28 +0530

    Update orderModel.js

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     shippingInfo: {
         address: {
-            type: String, 
+            type: String,  
             required: true
         },
         city: { 
