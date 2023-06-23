b972e4fb8f84fcdf44419016528661f01fe60350
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 14:55:04 +0530

    Update AlertDialgBox.js

--- client/src/components/AlertDialgBox.js
+++ client/src/components/AlertDialgBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import Button from "@material-ui/core/Button";
+import Button from "@material-ui/core/Button"; 
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
