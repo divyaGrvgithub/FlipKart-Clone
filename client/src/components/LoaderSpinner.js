8713ad2c1a8854eb5da855adb12d86a2fa04ba78
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 14:55:28 +0530

    Update LoaderSpinner.js

--- client/src/components/LoaderSpinner.js
+++ client/src/components/LoaderSpinner.js
@@ -7,7 +7,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     height: "100%",
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "column", 
     alignItems: "center",
   },
 }));
