12441d24b2f35869129091275332f21a0f45fd1c
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 15:07:06 +0530

    Update DBConnection.js

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config({path:"./.env"});
 const _DBUrl =process.env.DB;
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true); 
 mongoose
   .connect("mongodb+srv://divyamala_:Dt25042000knp@divyamala.0cofsch.mongodb.net/Flipkart", {
     useNewUrlParser: true,
