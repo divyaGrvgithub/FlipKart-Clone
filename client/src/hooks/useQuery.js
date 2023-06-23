3694c9d862aa10b105bcce6b281f4c89012e3a93
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 14:56:34 +0530

    Update useQuery.js

--- client/src/hooks/useQuery.js
+++ client/src/hooks/useQuery.js
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 function useQuery() {
   const { search } = useLocation();
-
+ 
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
