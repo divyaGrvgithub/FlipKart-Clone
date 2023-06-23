34ec000a027ca7574a2fd1aadc1f7923072b41c7
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 14:48:09 +0530

    Update firebase.js

--- client/src/adapters/firebase.js
+++ client/src/adapters/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
+const firebaseConfig = { 
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
