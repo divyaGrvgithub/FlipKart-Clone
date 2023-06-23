f75a2a3e6f0695b2e38d6fccbb225a34dd7e50d8
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-06-23 14:44:48 +0530

    Update addressActionType.js

--- client/src/action-type/addressActionType.js
+++ client/src/action-type/addressActionType.js
@@ -2,3 +2,4 @@ export const UPDATE_ADDRESS_COMPONENT_STATE = "updateAddressComponentState";
 export const ADD_NEW_ADDRESS = "addNewAddress";
 export const SET_ADDRESSES = "setAddresses";
 export const REMOVE_ADDRESS = "removeAddress";
+ 
